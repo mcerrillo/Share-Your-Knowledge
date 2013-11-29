080f7a756688a55a4fe8b542ccd743f4644e4732
Manuel Cerrillo <manuel.cerrillo.bermejo@gmail.com>
2013-11-29 21:05:22 +0100

    Version 4.3 Changes in sequelize version to run the app in Heroku

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ var GOOGLE_CLIENT_SECRET = "i9SyhZD6rqDKvsmXvXZFukzs";
 passport.use(new GoogleStrategy({
     clientID: GOOGLE_CLIENT_ID,
     clientSecret: GOOGLE_CLIENT_SECRET,
-    callbackURL: "http://localhost:3000/login/google/callback"
+    callbackURL: "share-your-knowledge-ltu.herokuapp.com/login/google/callback"
   },
   function(accessToken, refreshToken, profile, done) {
     // asynchronous verification, for effect...
