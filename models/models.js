f4be7e04056262c76ca20ec869cfcdcd30ac98d7
Daniel Buldon <d.buldon@gmail.com>
2013-11-29 15:21:53 +0100

    Version 4.1: Postgres configuration

--- models/models.js
+++ models/models.js
@@ -1,10 +1,16 @@
 var path = require('path');
 var Sequelize = require('sequelize');
 
-var sequelize = new Sequelize('mainDB', 'username', 'password', {
-  dialect: 'sqlite',
-  storage: 'mainDB.sqlite'
-})
+var sequelize = new Sequelize(process.env.DATABASE_NAME, 
+                              process.env.DATABASE_USER, 
+                              process.env.DATABASE_PASSWORD, 
+					            { dialect: process.env.DATABASE_DIALECT, 
+					              protocol: process.env.DATABASE_PROTOCOL, 
+					              port: process.env.DATABASE_PORT,
+					              host: process.env.DATABASE_HOST,
+					              storage: process.env.DATABASE_STORAGE,
+					              omitNull: true
+					          	});
 
 var User        = sequelize.import(path.join(__dirname,'user'));
 var UserContent = sequelize.import(path.join(__dirname,'user_content'));
