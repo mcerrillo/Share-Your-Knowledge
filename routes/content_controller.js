8ef99b034311de065bed4fdbc2582cf1ecf01c39
Manuel Cerrillo <manuel.cerrillo.bermejo@gmail.com>
2013-12-02 17:39:47 +0100

    Version 4.7.8 changes in the acces to the data base Authorized

--- routes/content_controller.js
+++ routes/content_controller.js
@@ -10,13 +10,13 @@ exports.show = function(req, res, next) {
 
 		  models.Authorized
 		  	.findAll({where: {email: req.session.passport.user.emails[0].value},
-		  			  include: [ { model: models.UserContent, as: 'content' } ],
-	             	  order: 'updatedAt DESC'})
+	             	  order: 'updatedAt DESC',
+	             	  include: [ { model: models.UserContent, as: 'content' } ]})
 		  	.success(function(authorized_contents){
 		  		res.render('index',{ render_body: 'profile', userName: req.session.passport.user.displayName, own_contents: own_contents, authorized_contents: authorized_contents, fl: req.flash()});
 		  	})
 		  	.error(function(error) {
-	       		//next(error);
+	       		next(error);
 	   		})
 	   })
 	   .error(function(error) {
