00c2f72fb6d440a4db1362de69fac00f271cd9e8
Manuel Cerrillo <manuel.cerrillo.bermejo@gmail.com>
2013-12-02 18:43:10 +0100

    Version 4.7.13

--- routes/content_controller.js
+++ routes/content_controller.js
@@ -11,7 +11,7 @@ exports.show = function(req, res, next) {
 		  models.Authorized
 		  	.findAll({where: {email: req.session.passport.user.emails[0].value},
 	             	  order: 'updatedAt DESC',
-	             	  include: [ { model: models.UserContent, as: 'content' } ]})
+	             	  include: [ {model: models.UserContent, as: 'content'} ]})
 		  	.success(function(authorized_contents){
 		  		res.render('index',{ render_body: 'profile', userName: req.session.passport.user.displayName, own_contents: own_contents, authorized_contents: authorized_contents, fl: req.flash()});
 		  	})
