423fbc894206bcc1911a1882d297cb03a410cd33
Manuel Cerrillo <manuel.cerrillo.bermejo@gmail.com>
2013-12-02 17:33:10 +0100

    Version 4.7.6

--- routes/content_controller.js
+++ routes/content_controller.js
@@ -16,11 +16,11 @@ exports.show = function(req, res, next) {
 		  		res.render('index',{ render_body: 'profile', userName: req.session.passport.user.displayName, own_contents: own_contents, authorized_contents: authorized_contents, fl: req.flash()});
 		  	})
 		  	.error(function(error) {
-	       		next(error);
+	       		//next(error);
 	   		})
 	   })
 	   .error(function(error) {
-	       //next(error);
+	       next(error);
 	   })
 };
 
